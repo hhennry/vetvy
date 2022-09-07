dd1e84bd869ca0b3b6c1196cccf7af5d03c147c4
Henry Rodrigues <108133435+hhennry@users.noreply.github.com>
2022-09-07 10:54:13 -0300

    <!-- B1.0.1 -->
    Limpeza de dependências não utilizadas e criação das primeiras estruturas das paginas.

---
+++ config/src/contents/an5Yl7e9.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function W0u19() {
+  return (
+    <header className="_4Zt4B161 w-screen h-72 absolute inset-0">
+      <div className="grid place-contents-center gap-1 text-center mt-20">
+        <h1 className="_nnP030JW">
+          vetvy<i className="_Y1rrP236">./</i>dev
+        </h1>
+        <h2 className="_7O7a88Vj">@hhennryy</h2>
+      </div>
+    </header>
+  );
+}
